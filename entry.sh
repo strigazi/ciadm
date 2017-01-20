f6ce256d1bd04890a627828f7d62cf1c2f04d691
Ricardo Rocha <rocha.porto@gmail.com>
2017-01-20 15:12:57 +0100

    set proper afs directory using user first letter
    was hardcoded to 'r', need to set it to the first letter of the username
    before cding into the afs directory.

--- entry.sh
+++ entry.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+FIRSTLETTER=$(echo $AFS_USER | cut -c 1)
 if [ ! -z ${AFS_USER} ]; then
 	/usr/bin/aklog
-	cd /afs/cern.ch/user/r/$AFS_USER
+	cd /afs/cern.ch/user/$FIRSTLETTER/$AFS_USER
 fi
 /bin/bash
